a638ede63e9754257a0ff2ba01687aa47ccc3aa9
pablo.lopez.socias@enti.cat <pablo.lopez.socias@enti.cat>
2022-12-04 18:25:52 +0100

    Memes a cholon

---
+++ client.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+
+echo ""
+
+if [ "$1" == "-h" ]
+then
+SCRIPT=`basename $0` #Se encarga de poner soloel nombre del script 
+	echo "Ejemplo de uso"
+	echo "$SCRIPT 127.0.0.1"
+	exit 0
+fi
+
+IP_SERVER="localhost"
+IP_LOCAL=`ip a | grep inet | grep enp0s3 | sed "s/^ *//g" | cut -d " " -f 2 | cut -d "/" -f 1`
+
+PORT="4242"
+
+echo "La IP local es: $IP_LOCAL"
+
+MD5_IP=`echo $IP_LOCAL | md5sum | cut -d " " -f 1`
+
+
+if [ "$1" != "" ]
+then 
+	IP_SERVER=$1
+	echo "La Ip del servidor es: $IP_SERVER"
+fi
+
+echo "Cliente HMTP"
+
+echo "(1) ENVIANDO EL SALUDO"
+
+echo "GREEN_POWA $IP_LOCAL $MD5_IP" | nc $IP_SERVER $PORT 
+
+echo "(2) RECIBIENDO CONFIRMACION"
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_HMTP" ]
+then 
+	echo "ERROR 1:SALUDO MAL HECHO"
+	exit 1
+fi
+
+echo "(5) CONTANDO Y ENVIANDO CONTEO"
+
+FILE_COUNT=`ls memes/ | wc -l`
+
+echo "$FILE_COUNT" | nc $IP_SERVER $PORT
+
+echo "(6) ESCUCHANDO CONFIRMACION DE CONTEO"
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_FILE_COUNT" ]
+	then 
+	echo "ERROR 2: NUMERO ERRONEO"
+	exit 2
+fi
+
+echo " "
+for ((i=0; i<=$FILE_COUNT-1; i++))
+do
+FILE_NAME="ElonMusk$i.jpg"
+
+echo "(9) ENVIANDO MENSAJE"
+
+FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+
+echo "(10) ESCUCHANDO"
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_FILENAME" ]
+then
+	echo "ERROR 3: EL NOMBRE DEL ARCHIVO ES ERRONEO"
+	echo "Mensaje de error: $MSG"
+exit 3
+fi
+
+echo "(13) ENVIAMOS DATOS"
+
+cat memes/$FILE_NAME | nc $IP_SERVER $PORT
+
+DATA_MD5=`cat memes/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "(14) ESCUCHAMOS RESPUESTA" 
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_DATA_RCPT" ]
+then
+	echo "ERROR 4: DATOS INCORRECTOS"
+	exit 4
+fi
+
+echo "(17) ENVIAMOS CONFIRMACION DE ARCHIVO"
+
+echo $DATA_MD5 | nc $IP_SERVER $PORT
+
+echo "(18) RECIBIMOS CONFIRMACION"
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_DATA_MD5" ]
+then
+	echo "ERROR 5: DATOS FALSOS"
+	echo "MENSAJE DE ERROR: $MSG"
+ 	exit 5
+fi
+
+echo " "
+
+done
+
+echo "DATOS ENVIADOS CORRECTAMENTE"
+
+echo "FIN DEL ENVIO"
+exit 0
---
+++ server.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+echo ""
+
+PORT="4242"
+
+echo "Servidor HMTP" 
+
+echo "(0) LEVANTANDO EL SERVIDOR"
+
+IP_SERVER=`ip a | grep inet | grep enp0s3 | sed "s/^ *//g" | cut -d " " -f 2 |  cut -d "/" -f 1`
+
+echo "La IP del servidor es:  $IP_SERVER"
+
+MSG=`nc -l $PORT`
+HANDSHAKE=`echo $MSG | cut -d " " -f 1`
+IP_CLIENTE=`echo $MSG | cut -d " " -f 2`
+MD5_CLIENTE=`echo $MSG | cut -d " " -f 3`
+MD5_IP=`echo $IP_CLIENTE | md5sum | cut -d " " -f 1`
+
+echo "(3) ENVIANDO CONFIRMACION"
+if [ "$MD5_IP" != "$MD5_CLIENTE" ]
+then
+echo "ERROR 1: IP errónea" 
+exit 1
+fi
+
+if [ "$HANDSHAKE" != "GREEN_POWA" ]
+then
+	echo "KO_HMTP" | nc $IP_CLIENTE $PORT
+	exit 1
+fi
+
+echo "OK_HMTP" | nc $IP_CLIENTE $PORT
+
+echo "(4) ESCUCHANDO"
+
+MSG=`nc -l $PORT`
+FILE_COUNT=`echo $MSG`
+
+if [ "$MSG" != "$FILE_COUNT" ]
+then
+echo "KO_FILE_COUNT" | nc $IP_CLIENTE $PORT
+exit 2
+fi
+echo "OK_FILE_COUNT" | nc $IP_CLIENTE $PORT
+echo ""
+
+echo "(7) ENVIANDO CONFIRMACION DE CONTEO"
+for ((i=0; i<=$FILE_COUNT-1; i++))
+do
+echo "(8) ESCUCHANDO"
+
+MSG=`nc -l $PORT`
+PREFIX=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+FILE_MD5=`echo $MSG | cut -d " " -f 3`
+
+echo "(11) ENVIANDO CONFIRMACION"
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "KO_FILENAME" | nc $IP_CLIENTE $PORT
+	exit 3
+fi
+
+
+MD5SUM=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+if [ "$MD5SUM" != "$FILE_MD5" ]
+then
+	echo "KO_FILE_MD5" | nc $IP_CLIENTE $PORT
+	exit 4
+fi
+
+echo "OK_FILENAME" | nc $IP_CLIENTE $PORT
+
+echo "(12) ESCUCHAMOS ARCHIVO"
+
+nc -l $PORT > inbox/$FILE_NAME
+
+DATA_MD5=`cat inbox/$FILE_NAME | md5sum | cut -d " " -f 1`
+echo "(15) ENVIAR CONFIRMCION" 
+
+echo "OK_DATA_RCPT" | nc $IP_CLIENTE $PORT
+
+echo "(16) ESCUCHANDO CONFIRMACION DE ARCHIVO"
+
+MSG=`nc -l $PORT`
+if [ "$MSG" != "$DATA_MD5" ]
+then
+	echo "KO_DATA_MD5" | nc $IP_CLIENTE $PORT
+	exit 5
+fi
+
+echo "OK_DATA_MD5" | nc $IP_CLIENTE $PORT
+echo " "
+done
+
+echo "DATOS RECIBIDOS CORRECTAMENTE"
+
+echo "FIN DE LA RECEPCION"
+
+exit 0
